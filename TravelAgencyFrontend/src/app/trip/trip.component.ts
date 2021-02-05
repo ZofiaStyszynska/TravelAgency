d13281a8634d3c5080658c1b765cb8f17e2b59a2
Agata <agatabienkowska1@wp.pl>
2021-02-05 09:04:36 +0100

    Complementing the trip component.

--- TravelAgencyFrontend/src/app/trip/trip.component.ts
+++ TravelAgencyFrontend/src/app/trip/trip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TRIPS} from "../mock-trips";
 
 @Component({
   selector: 'app-trip',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TripComponent implements OnInit {
 
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
