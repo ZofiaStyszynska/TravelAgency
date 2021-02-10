a41d2c3d38d95512527e8cca42041e0ab5be762c
ZofiaStyszynska <ZofiaStyszynska@users.noreply.github.com>
2021-02-10 12:07:55 +0100

    angular animations

--- TravelAgencyFrontend/src/app/app.module.ts
+++ TravelAgencyFrontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule,Provider} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -9,7 +9,8 @@ import {MainSiteComponent} from './main-site/main-site.component';
 import {UsersAccountComponent} from './users-account/users-account.component';
 import {FormsModule} from "@angular/forms";
 import {Route, RouterModule, Routes} from "@angular/router";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ANIMATION_MODULE_TYPE, BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 import {MatSliderModule} from '@angular/material/slider';
 import {MatCardModule} from "@angular/material/card";
 import {UserListComponent} from './users-account/user-list/user-list.component';
@@ -58,10 +59,12 @@ const routes: Routes = [
     MatSliderModule,
     MatCardModule,
     HttpClientModule,
-    MatInputModule
+    MatInputModule,
+    NoopAnimationsModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
